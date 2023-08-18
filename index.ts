eef3be588a72287f73557d84830fc952ed03da82
alihaidergithub <vergefuture@gmail.com>
2023-08-18 20:21:45 +0500

    18 August 2023
    18 August 2023

--- index.ts
+++ index.ts
@@ -219,21 +219,16 @@ let user = inquirer.prompt({
 })
 console.log(user)
 
-
 // Inquirer
 // Commonjs
 // Es
 // chalk(console)
 // banner(console)
 
-
 // Tasks for this week!!
 
-// 1. Create a modular calculator program using TypeScript. The program should take user input through the Inquirer library and implement various arithmetic operations (addition, subtraction, multiplication, division) as separate ES modules. 
+// 1. Create a modular calculator program using TypeScript. The program should take user input through the Inquirer library and implement various arithmetic operations (addition, subtraction, multiplication, division) as separate ES modules.
 
-// 2. Create a quiz application using TypeScript and the Inquirer library. The program should take user input through Inquirer, implement a quiz with a variable number of questions, calculate the quiz score in separate ES module, and display the final result along with correct and incorrect user-given answers. 
+// 2. Create a quiz application using TypeScript and the Inquirer library. The program should take user input through Inquirer, implement a quiz with a variable number of questions, calculate the quiz score in separate ES module, and display the final result along with correct and incorrect user-given answers.
 
 // Note: you can also use Chalk library for adding color to the console output.
-
-
-
