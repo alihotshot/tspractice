56b0be204c59477713241bdc71b676fe1828bf13
Ali <vergefuture@gmail.com>
2023-08-26 23:19:54 +0500

    26 Class
    26 Class

--- clsIndex.ts
+++ clsIndex.ts
@@ -1,4 +1,11 @@
+//Case 1 : When both are same
 
+let freshObject = { id: 50, firstName: 'Ali' }
+let secondObject = { id: 66, firstName: 'Haider' }
 
+freshObject = secondObject
 
+//Case 2 : When second one is different
 
+let newObject = { new_id: 65, firstName: 'Haider' }
+freshObject = newObject
